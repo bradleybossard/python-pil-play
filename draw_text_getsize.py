c5d759f6e03a2b2d429a60ad58afa3fa3e75e07d
Bradley Bossard <bradleybossard@gmail.com>
2013-09-27 14:14:07 -0700

    Added documentation for example

--- draw_text_getsize.py
+++ draw_text_getsize.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
+""" Drawing text that exactly fits inside a rectangle"""
 
 def draw_text(text, size, fill=None):
-    #font = ImageFont.truetype('path/to/font.ttf', size)
     font = ImageFont.truetype('/Library/Fonts/Impact.ttf', 30)
     size = font.getsize(text) # Returns the width and height of the given text, as a 2-tuple.
     im = Image.new('RGBA', size, (255, 0, 0, 255)) # Create a blank image with the given size
